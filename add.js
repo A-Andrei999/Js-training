84573b8dce6f65939c9599a2500dfca77927ec99
A-Andrei999 <74667061+A-Andrei999@users.noreply.github.com>
2021-01-19 21:14:33 +0300

    new

--- add.js
+++ add.js
@@ -1,28 +1,32 @@
-var imputCalc = document.querySelector('.histori');
-    resultCalc = document.querySelector('.output');
-
+var imputCalc = document.querySelector(".histori");
+resultCalc = document.querySelector(".output");
 
 function imput(i) {
-   imputCalc.value = imputCalc.value + i;
+  imputCalc.value = imputCalc.value + i;
 }
 
 function result() {
-   if( eval(imputCalc.value) == undefined){
-      resultCalc.value = "0";
-      imputCalc.value ="0";
-   } if( eval(imputCalc.value) == Infinity){
-      resultCalc.value = "0";
-      imputCalc.value ="You cannot divide by zero";
-   }
-   resultCalc.value = eval(imputCalc.value);
-   imputCalc.value = eval(imputCalc.value);
+  if (eval(imputCalc.value) == undefined) {
+    resultCalc.value = "0";
+    imputCalc.value = "0";
+  }
+  if (eval(imputCalc.value) == Infinity) {
+    resultCalc.value = "0";
+    imputCalc.value = "You cannot divide by zero";
+  }
+  resultCalc.value = eval(imputCalc.value);
+  imputCalc.value = eval(imputCalc.value);
 }
 
 function backspace() {
-   imputCalc.value = imputCalc.value.substring(0, imputCalc.value.length-1 )
+  imputCalc.value = imputCalc.value.substring(0, imputCalc.value.length - 1);
 }
 
 function reset() {
-   resultCalc.value = "0";
-   imputCalc.value ="0";
+  resultCalc.value = "";
+  imputCalc.value = "";
+}
+function percent() {
+  procent = imputCalc.value - imputCalc.value * (i / 100);
+  imputCalc.value = procent;
 }
